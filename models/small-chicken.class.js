2d57df015d80e8203f091369d7a0c4a392246439
Karina Tanei <karinataneis@gmail.com>
2024-07-17 12:42:30 +0200

    chicken classes add dead picture

--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -2,23 +2,22 @@ class smallChicken extends MovableObject {
     width = 60;
     height = 60;
     y = 375;
-    IMAGES_WALKING = [
+    IMAGES_SMALLCHICKEN_WALKING = [
         './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
         './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
         './img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
     ];
 
+    IMAGES_SMALLCHICKEN_DEAD = [
+        './img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+    ];
     // walking_sound = new Audio('./audio/enemyTalking.mp3');
     
-
    constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
-        this.loadImages(this.IMAGES_WALKING);
-        
+        this.loadImages(this.IMAGES_SMALLCHICKEN_WALKING);
         this.x = 2500 + Math.random() * 1000;
         this.speed = 1.5 + Math.random() * 0.5;
-        
-
         this.animate();
     }
 
@@ -27,11 +26,20 @@ class smallChicken extends MovableObject {
             this.moveLeft();
         }, 1000 / 60);
 
-        
-
         setInterval(() => {
-            this.playAnimation(this.IMAGES_WALKING);
+            this.playAnimation(this.IMAGES_SMALLCHICKEN_WALKING);
             // this.walking_sound.play();
         }, 200);
     }
 }
+
+//     animate() {
+//         setInterval(() => {
+//         if (this.isDead()) {
+//             this.playAnimation(this.IMAGES_BOSS_DEAD);
+//         } else {
+//             this.playAnimation(this.IMAGES_WALKING);
+//         }
+//     }, 200);
+//  }
+
