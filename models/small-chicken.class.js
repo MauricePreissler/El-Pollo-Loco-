31e73b67b814f495581a439d670279e24d9ba755
BuenyaminTig <buenyamin.tig@gmail.com>
2024-07-18 19:48:03 +0300

    smallchicken dead by jump

--- models/small-chicken.class.js
+++ models/small-chicken.class.js
@@ -1,3 +1,93 @@
+// class smallChicken extends MovableObject {
+//     width = 60;
+//     height = 60;
+//     y = 375;
+//     IMAGES_SMALLCHICKEN_WALKING = [
+//         './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+//         './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+//         './img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+//     ];
+
+//     IMAGES_SMALLCHICKEN_DEAD = [
+//         './img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+//     ];
+    
+//    constructor() {
+//         super().loadImage('./img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+//         this.loadImages(this.IMAGES_SMALLCHICKEN_WALKING);
+//         this.x = 2500 + Math.random() * 1000;
+//         this.speed = 0.5 + Math.random() * 0.5;
+//         this.animate();
+//     }
+
+//     animate() {
+//         setInterval(() => {
+//             this.moveLeft();
+//         }, 1000 / 60);
+
+//         setInterval(() => {
+//             this.playAnimation(this.IMAGES_SMALLCHICKEN_WALKING);
+//         }, 200);
+//     }
+// }
+
+// class smallChicken extends MovableObject {
+//     width = 60;
+//     height = 60;
+//     y = 375;
+//     IMAGES_SMALLCHICKEN_WALKING = [
+//         './img/3_enemies_chicken/chicken_small/1_walk/1_w.png',
+//         './img/3_enemies_chicken/chicken_small/1_walk/2_w.png',
+//         './img/3_enemies_chicken/chicken_small/1_walk/3_w.png'
+//     ];
+
+//     IMAGES_SMALLCHICKEN_DEAD = [
+//         './img/3_enemies_chicken/chicken_small/2_dead/dead.png'
+//     ];
+
+//     isDead = false;
+
+//     constructor() {
+//         super().loadImage('./img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
+//         this.loadImages(this.IMAGES_SMALLCHICKEN_WALKING);
+//         this.loadImages(this.IMAGES_SMALLCHICKEN_DEAD);
+        
+//         this.x = 2500 + Math.random() * 1000;
+//         this.speed = 0.5 + Math.random() * 0.5;
+//         this.animate();
+//     }
+
+//     animate() {
+//         this.moveLeftInterval = setInterval(() => {
+//             if (!this.isDead) {
+//                 this.moveLeft();
+//             }
+//         }, 1000 / 60);
+
+//         this.animationInterval = setInterval(() => {
+//             if (this.isDead) {
+//                 this.playAnimation(this.IMAGES_SMALLCHICKEN_DEAD);
+//             } else {
+//                 this.playAnimation(this.IMAGES_SMALLCHICKEN_WALKING);
+//             }
+//         }, 200);
+//     }
+
+//     stopMovement() {
+//         clearInterval(this.moveLeftInterval);
+//         this.isDead = true;
+//         this.fadeOut();
+//     }
+
+//     fadeOut() {
+//         let fadeOutInterval = setInterval(() => {
+//             this.y -= 2; // Bewegt das Hähnchen langsam nach oben
+//             if (this.y + this.height < 0) {
+//                 clearInterval(fadeOutInterval);
+//             }
+//         }, 1000 / 60);
+//     }
+// }
 class smallChicken extends MovableObject {
     width = 60;
     height = 60;
@@ -11,32 +101,47 @@ class smallChicken extends MovableObject {
     IMAGES_SMALLCHICKEN_DEAD = [
         './img/3_enemies_chicken/chicken_small/2_dead/dead.png'
     ];
-    
-   constructor() {
+
+    isDead = false;
+
+    constructor() {
         super().loadImage('./img/3_enemies_chicken/chicken_small/1_walk/1_w.png');
         this.loadImages(this.IMAGES_SMALLCHICKEN_WALKING);
+        this.loadImages(this.IMAGES_SMALLCHICKEN_DEAD);
+        
         this.x = 2500 + Math.random() * 1000;
         this.speed = 0.5 + Math.random() * 0.5;
         this.animate();
     }
 
     animate() {
-        setInterval(() => {
-            this.moveLeft();
+        this.moveLeftInterval = setInterval(() => {
+            if (!this.isDead) {
+                this.moveLeft();
+            }
         }, 1000 / 60);
 
-        setInterval(() => {
-            this.playAnimation(this.IMAGES_SMALLCHICKEN_WALKING);
+        this.animationInterval = setInterval(() => {
+            if (this.isDead) {
+                this.playAnimation(this.IMAGES_SMALLCHICKEN_DEAD);
+            } else {
+                this.playAnimation(this.IMAGES_SMALLCHICKEN_WALKING);
+            }
         }, 200);
     }
-}
 
-//     animate() {
-//         setInterval(() => {
-//         if (this.isDead()) {
-//             this.playAnimation(this.IMAGES_BOSS_DEAD);
-//         } else {
-//             this.playAnimation(this.IMAGES_WALKING);
-//         }
-//     }, 200);
-//  }
+    stopMovement() {
+        clearInterval(this.moveLeftInterval);
+        this.isDead = true;
+        this.fadeOut();
+    }
+
+    fadeOut() {
+        let fadeOutInterval = setInterval(() => {
+            this.y += 2; // Bewegt das Hähnchen langsam nach unten
+            if (this.y + this.height < 0) {
+                clearInterval(fadeOutInterval);
+            }
+        }, 1000 / 60);
+    }
+}
--- models/world.class.js
+++ models/world.class.js
@@ -1,3 +1,171 @@
+// class World {
+//     character = new Character();
+//     endboss = new Endboss();
+//     throwableObject = new ThrowableObject();
+//     level = level1;
+//     canvas;
+//     ctx;
+//     keyboard;
+//     camera_x = 0;
+//     statusBar = new StatusBar();
+//     StatusBarBottles = new StatusBarBottles();
+//     StatusBarCoins = new StatusBarCoins();
+//     StatusBarEndboss = new StatusBarEndboss();
+//     CollectBottles = new CollectBottles();
+//     collectedCoins = [];
+//     collectedBottles = [];
+//     throwableObjects = [];
+
+//     constructor(canvas, keyboard) {
+//         this.ctx = canvas.getContext('2d');
+//         this.canvas = canvas;
+//         this.keyboard = keyboard;
+//         this.draw();
+//         this.setWorld();
+//         this.run();
+//     }
+
+//     setWorld() {
+//         this.character.world = this;
+//     }
+
+//     run() {
+//         setInterval(() => {
+//             this.checkCollisions();
+//             this.checkThrowObjects();
+//         }, 200);
+//     }
+
+//     checkThrowObjects() {
+//         if (this.keyboard.D && this.collectedBottles.length > 0) {
+//             let bottle = new ThrowableObject(this.character.x + 100, this.character.y + 100);
+//             this.throwableObjects.push(bottle);
+//             this.collectedBottles.pop(); // Entfernt eine Flasche aus der Sammlung
+//             this.StatusBarBottles.setPercentage((this.collectedBottles.length / 5) * 100); // Aktualisiert die Statusleiste
+//         }
+//     }
+
+    
+    
+//     checkCollisions() {
+//         this.level.enemies.forEach((enemy) => {
+//             if (this.character.isColliding(enemy)) {
+//                 if (enemy instanceof Chicken) {
+//                     if (enemy.isDead) {
+//                         // Kein Schaden, wenn das Hähnchen tot ist
+//                         return;
+//                     }
+//                     if (this.character.y + this.character.height <= enemy.y + enemy.height / 2) {
+//                         // Der Charakter springt von oben auf das Hähnchen
+//                         enemy.stopMovement(); // Stoppt die Bewegung des Hähnchens
+//                         console.log('Character hit the Chicken from above');
+//                     } else {
+//                         this.character.hit(); // Der Charakter nimmt Schaden
+//                         this.statusBar.setPercentage(this.character.energy); // Aktualisiert die Statusleiste
+//                     }
+//                 } else {
+//                     this.character.hit(); // Der Charakter nimmt Schaden
+//                     this.statusBar.setPercentage(this.character.energy); // Aktualisiert die Statusleiste
+//                 }
+//             }
+//         });
+    
+//         this.throwableObjects.forEach((throwableObject) => {
+//             if (throwableObject.isColliding(this.endboss)) {
+//                 this.endboss.attack();
+//                 this.StatusBarEndboss.setPercentage(this.endboss.energy);
+//                 console.log('Throwable Object hit the Endboss', this.endboss.energy);
+//             }
+//         });
+    
+//         if (this.character.isColliding(this.endboss)) {
+//             this.character.endbossAttackCharacter();
+//             this.statusBar.setPercentage(this.character.energy);
+//             console.log('Endboss hit the Character', this.character.energy);
+//         }
+    
+//         this.level.coins.forEach((coin) => {
+//             if (this.character.isColliding(coin)) {
+//                 console.log('Character hit the Coins');
+//                 this.collectedCoins.push(coin);
+//                 this.level.coins.splice(this.level.coins.indexOf(coin), 1); // Entfernt die Münze aus dem Level
+//                 this.StatusBarCoins.setPercentage(this.collectedCoins.length, 5); // Setze 5 als die Gesamtzahl der Münzen
+//             }
+//         });
+    
+//         this.level.bottles.forEach((bottle) => {
+//             if (this.character.isColliding(bottle) && this.collectedBottles.length < 5) {
+//                 console.log('Character hit the Bottles');
+//                 this.collectedBottles.push(bottle);
+//                 this.level.bottles.splice(this.level.bottles.indexOf(bottle), 1); // Entfernt die Flasche aus dem Level
+//                 this.StatusBarBottles.setPercentage((this.collectedBottles.length / 5) * 100); // Aktualisiert die Statusleiste
+//             }
+//         });
+//     }
+    
+
+//     draw() {
+//         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
+
+//         this.ctx.translate(this.camera_x, 0);
+//         this.addObjectsToMap(this.level.backgroundObjects);
+
+//         this.ctx.translate(-this.camera_x, 0);
+//         this.addToMap(this.statusBar);
+//         this.addToMap(this.StatusBarCoins);
+//         this.addToMap(this.StatusBarBottles);
+//         this.addToMap(this.StatusBarEndboss);
+//         this.ctx.translate(this.camera_x, 0);
+
+//         this.addObjectsToMap(this.level.bottles);
+//         this.addObjectsToMap(this.level.coins);
+
+//         this.addToMap(this.endboss);
+//         this.addToMap(this.character);
+//         this.addObjectsToMap(this.level.enemies);
+//         this.addObjectsToMap(this.level.clouds);
+//         this.addObjectsToMap(this.throwableObjects);
+
+//         this.ctx.translate(-this.camera_x, 0);
+
+//         let self = this;
+//         requestAnimationFrame(function() {
+//             self.draw();
+//         });
+//     }
+
+//     addObjectsToMap(objects) {
+//         objects.forEach(o => {
+//             this.addToMap(o);
+//         });
+//     }
+
+//     addToMap(mo) {
+//         if (mo.otherDirection) {
+//             this.flipImage(mo);
+//         }
+
+//         mo.draw(this.ctx);
+//         mo.drawFrame(this.ctx);
+
+//         if (mo.otherDirection) {
+//             this.flipImageBack(mo);
+//         }
+//     }
+
+//     flipImage(mo) {
+//         this.ctx.save();
+//         this.ctx.translate(mo.width, 0);
+//         this.ctx.scale(-1, 1);
+//         mo.x = mo.x * -1;
+//     }
+
+//     flipImageBack(mo) {
+//         mo.x = mo.x * -1;
+//         this.ctx.restore();
+//     }
+// }
+
 class World {
     character = new Character();
     endboss = new Endboss();
@@ -45,12 +213,10 @@ class World {
         }
     }
 
-    
-    
     checkCollisions() {
         this.level.enemies.forEach((enemy) => {
             if (this.character.isColliding(enemy)) {
-                if (enemy instanceof Chicken) {
+                if (enemy instanceof Chicken || enemy instanceof smallChicken) {
                     if (enemy.isDead) {
                         // Kein Schaden, wenn das Hähnchen tot ist
                         return;
@@ -69,7 +235,7 @@ class World {
                 }
             }
         });
-    
+
         this.throwableObjects.forEach((throwableObject) => {
             if (throwableObject.isColliding(this.endboss)) {
                 this.endboss.attack();
@@ -77,13 +243,13 @@ class World {
                 console.log('Throwable Object hit the Endboss', this.endboss.energy);
             }
         });
-    
+
         if (this.character.isColliding(this.endboss)) {
             this.character.endbossAttackCharacter();
             this.statusBar.setPercentage(this.character.energy);
             console.log('Endboss hit the Character', this.character.energy);
         }
-    
+
         this.level.coins.forEach((coin) => {
             if (this.character.isColliding(coin)) {
                 console.log('Character hit the Coins');
@@ -92,7 +258,7 @@ class World {
                 this.StatusBarCoins.setPercentage(this.collectedCoins.length, 5); // Setze 5 als die Gesamtzahl der Münzen
             }
         });
-    
+
         this.level.bottles.forEach((bottle) => {
             if (this.character.isColliding(bottle) && this.collectedBottles.length < 5) {
                 console.log('Character hit the Bottles');
@@ -102,7 +268,6 @@ class World {
             }
         });
     }
-    
 
     draw() {
         this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
@@ -165,5 +330,6 @@ class World {
         this.ctx.restore();
     }
 }
+
 
 
